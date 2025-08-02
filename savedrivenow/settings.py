09f5512fd51f3b1149d4c22b246a6d208e9b058b
Simon Bermudez <iam@simonbermudez.com>
2025-08-02 09:02:25 -0400

    fix: Update STATIC_ROOT and STATICFILES_DIRS for correct static file handling

--- savedrivenow/settings.py
+++ savedrivenow/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Additional locations of static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 # Static files finders
