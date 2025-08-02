45b8b147ed2d910e70d1596c36eafbd45fd28cce
Simon Bermudez <iam@simonbermudez.com>
2025-08-02 09:06:44 -0400

    fix: Update database settings to use PostgreSQL with environment variables

--- savedrivenow/settings.py
+++ savedrivenow/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'savedrivenow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'savedrivenow'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
